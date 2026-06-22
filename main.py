88d861e545ece7c58442b1e4fa65d9c213882fe5
Blue Steve <bluesteve1926@gmail.com>
2026-06-22 21:18:15 +1200

    main_V1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: Ryan Fernando
 Date 8/05/2026
 Purpose: To create a program using complex techniques to help students improve their math skills by creating an interactive quiz-like math game.
-Version: 2.0
+Version: 1.0
 """
 
 # ===== Modules imported =====
@@ -28,7 +28,7 @@ This gets the absolute path of the files' directory, so it opens the files which
 It is important so the program can run properly while getting all the relevant files,
 regardless of which device it is being run on and where or how the program is run.
 """
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # ===== Main window =====
 
@@ -36,55 +36,15 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 class App(tk.Tk):
     # Main window
     def __init__(self):
-        """
-        Since 'tk.Tk' is the main class' parameter, this tells the program that 'self' is reffered to it in order to set the GUI.
-        This is similar to declaring a 'root' variable as 'tk.Tk()'.
-        """
+        # Since 'tk.Tk' is the main class' parameter, this tells the program that 'self' is reffered to it in order to set the GUI. This is similar to declaring a 'root' variable as 'tk.Tk()'.
         super().__init__()
         self.title("Flow Computing")
         self.state('zoomed')
 
-        # Get app favicon path and load it to replace the default tkinter favicon
-        app_favicon = tk.PhotoImage(file=os.path.join(BASE_DIR, "images", "app_favicon.png"))
+        # Get app favicon path
+        app_favicon = tk.PhotoImage(file=os.path.join(base_dir, "images", "app_favicon.png"))
+        # Load favicon
         self.iconphoto(True, app_favicon)
-
-        # Frame container for the app's pages
-        page_container = tk.Frame(self)
-        page_container.pack(fill="both", expand=True)
-
-        # Array to store each page
-        self.pages = {}
-
-        # Create pages and add them to the array defined above
-        for P in (WelcomeScreen, LobbyScreen):
-            page = P(page_container, self)
-            self.pages[P] = page
-            page.grid(row=0, column=0, sticky="nsew")
-        
-        # Display welcome screen when program is first executed
-        self.display_page(WelcomeScreen)
-
-    # Function to display specified page
-    def display_page(self, page_class):
-        page = self.pages[page_class]
-        page.tkraise()
-
-# ===== Classes for each page =====
-# These are displayed via the main app class
-
-# Welcome Screen window containing the login, signup, and quit buttons
-class WelcomeScreen(tk.Frame):
-    def __init__(self, parent, controller):
-        # Calls the parent class 'tk.Frame' to initialise the page's frame
-        super().__init__(parent)
-
-# Lobby screen containing the difficulty levels, leaderboard button, and button to view user's previous scores
-class LobbyScreen(tk.Frame):
-    def __init__(self, parent, controller):
-        # Calls the parent class 'tk.Frame' to initialise the page's frame
-        super().__init__(parent)
-
-
 
 # Initialises the code when the program starts running, and puts the GUI window into a permanent loop to prevent it from closing.
 if __name__ == "__main__":
